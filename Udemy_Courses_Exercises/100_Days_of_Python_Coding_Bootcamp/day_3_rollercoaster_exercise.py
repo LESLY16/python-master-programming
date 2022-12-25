336e45e47f9bd2a2ad5b59deba8399daaaec5071
Lesly Max Dsouza <lesly1996max@gmail.com>
2022-12-25 19:13:31 +0530

    Trying to push day3 exercise file

---
+++ Udemy_Courses_Exercises/100_Days_of_Python_Coding_Bootcamp/day_3_rollercoaster_exercise.py
@@ -0,0 +1,6 @@
+print("Welcome to rollercoaster ride!")
+height = int(input("How tall are you in cm?"))
+if height >= 120:
+  print("Hurrah! You can ride the rollercoaster.")
+else:
+  print("Sorry, you have to grow taller to ride the rollercoaster")
